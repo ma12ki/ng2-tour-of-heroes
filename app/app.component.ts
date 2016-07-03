cc9204aed244919e907bdad7a76a2e6fc00f2863
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-07-03 12:45:54 +0200

    finished part 5 - services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+const HEROES: Hero[] = [];
 
 @Component({
   selector: 'my-app',
@@ -78,11 +68,25 @@ const HEROES: Hero[] = [
       border-radius: 4px 0 0 4px;
     }
   `],
-  directives: [HeroDetailComponent]
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
   public heroes = HEROES;
   title = 'Tour of Heroes';
   selectedHero: Hero;
-  onSelect(hero: Hero) { this.selectedHero = hero; }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero) { 
+    this.selectedHero = hero;
+   }
+
+  getHeroes() {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
 }
